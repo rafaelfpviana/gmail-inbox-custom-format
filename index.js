60ff111b6cd0298dde9bd2feee447e6117019747
Rafael <Rafael>
2020-04-02 17:08:11 -0300

    First files

---
+++ index.js
@@ -0,0 +1,26 @@
+const ib = require('./gmail-inbox');
+
+async function exeCuteMe(){
+  let inbox = new ib.Inbox('credentials.json');
+  await inbox.authenticateAccount(); // logs user in
+  
+  let messages = await inbox.findMessages({
+	category: 'promotions',
+	before: {
+		date: new Date(2020, 03, 31),
+		precision: 'day'
+	},
+	after: {
+		date: new Date(2020, 01, 01),
+		precision: 'day'
+	}
+  },200);
+
+  let messagesJson = JSON.stringify(messages, null, 2);
+
+  console.log(messagesJson);
+  
+  // Note: give  https://github.com/ismail-codinglab/gmail-inbox a star if it saved you time!
+}
+
+exeCuteMe();
