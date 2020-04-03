18aafa3ef8db1696a0d4ba72fa6ae6698090efa7
Rafael <Rafael>
2020-04-03 16:30:00 -0300

    Created custom format for message

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
 const ib = require('./gmail-inbox');
+const customFormat = require('./lib/src/CustomFormat');
 
-async function exeCuteMe(){
-  let inbox = new ib.Inbox('credentials.json');
-  await inbox.authenticateAccount(); // logs user in
-  
-  let messages = await inbox.findMessages({
-	category: 'promotions',
-	before: {
-		date: new Date(2020, 03, 31),
-		precision: 'day'
-	},
-	after: {
-		date: new Date(2020, 01, 01),
-		precision: 'day'
-	}
-  },200);
+async function exeCuteMe() {
+	let inbox = new ib.Inbox(
+		'credentials.json',
+		'gmail-token.json',
+		new customFormat.CustomFormat()
+	);
+	await inbox.authenticateAccount(); // logs user in
 
-  let messagesJson = JSON.stringify(messages, null, 2);
+	let messages = await inbox.findMessages({
+		category: 'promotions',
+		before: {
+			date: new Date(2020, 03, 31),
+			precision: 'day'
+		},
+		after: {
+			date: new Date(2020, 01, 01),
+			precision: 'day'
+		}
+	}, 200);
 
-  console.log(messagesJson);
-  
-  // Note: give  https://github.com/ismail-codinglab/gmail-inbox a star if it saved you time!
+	let messagesJson = JSON.stringify(messages, null, 2);
+
+	console.log(messagesJson);
+
+	// Note: give  https://github.com/ismail-codinglab/gmail-inbox a star if it saved you time!
 }
 
 exeCuteMe();
---
+++ src/CustomFormat.ts
@@ -0,0 +1,55 @@
+import { gmail_v1 } from 'googleapis';
+import { Message } from '../gmail-inbox/src/Message.interface';
+import { FormatMessageInterface } from '../gmail-inbox/src/FormatMessageInterface.interface';
+import { FormatMessage } from '../gmail-inbox/src/FormatMessage';
+
+interface CustomMessage extends Message {
+    xHeaders: Array<gmail_v1.Schema$MessagePartHeader> | undefined;
+    allHeaders: Array<gmail_v1.Schema$MessagePartHeader> | undefined;
+}
+
+export class CustomFormat extends FormatMessage implements FormatMessageInterface {
+    format(message: any): Message {
+        const headers = message.data.payload?.headers;
+        const xHeaders = this.getXHeaders(headers);
+        const mainHeaders = ['From', 'To', 'Subject', 'Date'];
+        const customMessage: CustomMessage = {
+            from: this.getHeader('From', headers),
+            to: this.getHeader('To', headers),
+            subject: this.getHeader('Subject', headers),
+            internalDate: message.data.internalDate!,
+            receivedOn: this.getHeader('Date', headers),
+            historyId: message.data.historyId!,
+            labelIds: message.data.labelIds!,
+            messageId: message.data.id!,
+            snippet: message.data.snippet!,
+            threadId: message.data.threadId!,
+            xHeaders: xHeaders,
+            allHeaders: this.getOtherHeaders(mainHeaders, xHeaders!.map(x => x.name!), headers)
+        }
+        return customMessage;
+    }
+
+    private getXHeaders(
+        headers: Array<gmail_v1.Schema$MessagePartHeader> | undefined
+    ): Array<gmail_v1.Schema$MessagePartHeader> | undefined {
+        if (!headers) {
+            return;
+        }
+        const found = headers.filter(h => h.name!.toLowerCase().startsWith('x-'));
+        return found;
+    };
+
+    private getOtherHeaders(
+        defaultH: Array<string>,
+        xH: Array<string>,
+        headers: Array<gmail_v1.Schema$MessagePartHeader> | undefined,
+    ): Array<gmail_v1.Schema$MessagePartHeader> | undefined {
+        if (!headers) {
+            return;
+        }
+        const excluded = defaultH.concat(xH);
+        return headers.filter(header => !excluded.includes(header.name!));
+    };
+
+}
